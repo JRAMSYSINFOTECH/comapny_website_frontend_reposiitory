fb27327d8594dca98936aa7f87a6263e2d173f6d
Manas Patil <89078720+Manaspatil30@users.noreply.github.com>
2025-08-04 13:17:08 +0100

    navbar css import

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,7 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold">JRAMSYS INFOTECH</h1>
      <Navbar />
-
-      <h1>About us</h1>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 'use client';
 import React from 'react';
+import "../styles/navbar.css";
 
 
 export default function Navbar() {
